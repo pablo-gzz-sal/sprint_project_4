6382ae24865d92e95e2d61251dd0546c8dcec64a
Pablo Gonzalez <pablo.gzz.sal@gmail.com>
2022-03-13 15:11:30 +0100

    commit on 'master'

---
+++ sprint_project_4/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import logo from "./svg/logo big.svg"
+
+function App() {
+  return (
+    <div className="App">
+      <img src={logo} alt="logo" />
+    </div>
+  );
+}
+
+export default App;
---
+++ sprint_project_4/src/components/login/Login.js
@@ -0,0 +1,14 @@
+import React from "react"
+import "./Login.css"
+import logo from "./svg/logo big.svg"
+
+function Login() {
+    return <div className="login">
+        <img src={logo} alt="logo" className="logo"/>
+        <div className="infoSection">
+            <h1></h1>
+            <p></p>
+            
+        </div>
+    </div>
+}
